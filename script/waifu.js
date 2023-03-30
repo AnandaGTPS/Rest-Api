d492a4073dfcc560f66d645f2b8ba9a67287bbe9
AnandaGTPS <108647595+AnandaGTPS@users.noreply.github.com>
2023-03-30 20:02:13 +0700

    Create waifu.js

---
+++ script/waifu.js
@@ -0,0 +1,10 @@
+const url = 'https://api.waifu.pics/sfw/waifu'; // Ganti dengan URL JSON yang ingin digunakan
+
+fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    const imageUrl = data.url;
+    const imgContainer = document.getElementById('img-container');
+    imgContainer.setAttribute('src', imageUrl);
+  })
+  .catch(error => console.error(error));
